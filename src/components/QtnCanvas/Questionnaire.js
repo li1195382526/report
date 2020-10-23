eb17413658a4ab62b4deeecccba0e68884f81e4c
li1195382526 <1195382526@qq.com>
2020-10-23 13:40:15 +0800

    必答显示

--- src/components/QtnCanvas/Questionnaire.js
+++ src/components/QtnCanvas/Questionnaire.js
@@ -26,7 +26,7 @@ class Questionnaire extends Component {
                   <View>
                     {qt.disSeq+'.'+qt.text}
                   </View>
-                  <View>（必填）</View>
+                  <View>{!!qt.required ? '（必填）':''}</View>
                 </View>
               </View>
                 {qt.type === 1 && <QuestionChoice  opts={qt}/>}
